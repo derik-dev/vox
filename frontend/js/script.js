820a9be4878e0060b785f17f3259ca675a8c4f62
Derik Oliveira <85948745+derik-dev@users.noreply.github.com>
2024-03-20 13:07:16 -0300

    Update script.js

--- frontend/js/script.js
+++ frontend/js/script.js
@@ -83,7 +83,7 @@ const handleLogin = (event) => {
     login.style.display = "none"
     chat.style.display = "flex"
 
-    websocket = new WebSocket("wss://vox-3.onrender.com")
+    websocket = new WebSocket("wss://vox-4ymm.onrender.com")
     websocket.onmessage = processMessage
 }
 
